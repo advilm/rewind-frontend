66f7311652bb18691709dac8872ae9fe6427bbb1
Adil Mohiuddin <amohi9046@gmail.com>
2021-03-08 19:38:12 -0800

    splitting into multiple files and adding home route

---
+++ components/Layout.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Head from 'next/head';
+import Nav from './Nav';
+
+export default function Layout({
+	children,
+	title = 'Rewind'
+}) {
+	return (
+		<div>
+			<Head>
+				<title>{title}</title>
+				<meta charSet="utf-8" />
+				<meta name="viewport" content="initial-scale=1.0, width=device-width" />
+			</Head>
+			<div className='flex flex-col bg-primary h-screen text-primary-text'>
+				<Nav />
+				{children}
+			</div>
+		</div>
+	);
+}
---
+++ components/Nav.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Link from 'next/link';
+
+export default function Nav() {
+	return (
+		<nav className='py-4 bg-primary-light text-xl'>
+			<div className='flex items-strech justify-between'>
+				<Link href="/">
+					<a>
+						<div className='flex items-center'>
+							<img src='favicon.ico' width='32' height='32' className='ml-4 mr-2'></img>
+							<strong>Rewind</strong>
+						</div>
+					</a>
+				</Link>
+				<div className='items-end'>
+					<a href='https://discord.gg/EbJzhBqu2U' target='_blank' rel='noreferrer' className='px-4 border-r'>Discord</a>
+					<a href='https://github.com/advilm/rewind-frontend' target='_blank' rel='noreferrer' className='px-4'>GitHub</a>
+				</div>
+			</div>
+		</nav>
+	);
+}
