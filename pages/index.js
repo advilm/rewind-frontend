5daa65fd325bccf974f008adff009707a84739d0
Adil Mohiuddin <amohi9046@gmail.com>
2021-03-09 22:14:56 -0800

    added app.js

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import 'tailwindcss/tailwind.css';
 import Layout from '../components/Layout';
 
+const url = 'https://discord.com/api/oauth2/authorize?client_id=639617043502727209&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fcallback&response_type=code&scope=identify%20guilds';
+// const url = 'https://discord.com/api/oauth2/authorize?client_id=639617043502727209&redirect_uri=https%3A%2F%2Frewind.advil.cf%2Fcallback&response_type=code&scope=identify%20guilds'
+
 export default function Home() {
 	return (
 		<Layout>
 			<div className='flex flex-col items-center text-center pt-24'>
 				<h1 className='text-5xl'>Press the button</h1>
 				<button className='duration-200 p-2 mt-4 rounded-md text-xl bg-secondary text-primary hover:bg-secondary-dark'>
-					<a href='https://discord.com/api/oauth2/authorize?client_id=639617043502727209&redirect_uri=https%3A%2F%2Frewind.advil.cf%2Fcallback&response_type=code&scope=identify%20guilds'>
+					<a href={url}>
 						Login with Discord
 					</a>
 				</button>
