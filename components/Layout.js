3c3c93e04ec4ec5f0e1d9f2809db85ccf7bc1412
Adil Mohiuddin <amohi9046@gmail.com>
2021-03-22 17:32:34 -0700

    added meta tags

--- components/Layout.js
+++ components/Layout.js
@@ -10,8 +10,16 @@ export default function Layout({
 		<div>
 			<Head>
 				<title>{title}</title>
-				<meta charSet='utf-8' />
-				<meta name='viewport' content='initial-scale=1.0, width=device-width' />
+				<meta charSet='utf-8'/>
+				<meta name='viewport' content='initial-scale=1.0, width=device-width'/>
+
+				<meta name="title" content="Rewind"/>
+				<meta name="description" content="Music bot with support for playlists and web dashboard for managing queue."/>
+
+				<meta property="og:type" content="website"/>
+				<meta property="og:url" content="https://rewind.advil.cf/"/>
+				<meta property="og:title" content="Rewind"/>
+				<meta property="og:description" content="Music bot with support for playlists and web dashboard for managing queue."/>
 			</Head>
 			<div className='flex flex-col bg-primary h-screen text-primary-text'>
 				<Nav />
