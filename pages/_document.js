5a0ea1e8f324510214caf98863e87fb4cdd20e1e
Adil Mohiuddin <amohi9046@gmail.com>
2021-03-22 18:33:05 -0700

    discord

--- pages/_document.js
+++ pages/_document.js
@@ -16,12 +16,12 @@ class MyDocument extends Document {
 					<meta name='viewport' content='initial-scale=1.0, width=device-width'/>
 
 					<meta name="title" content="Rewind"/>
-					<meta name="description" content="Music bot with support for playlists and web dashboard for managing queue."/>
+					<meta name="description" content="Discord music bot with support for playlists and web dashboard for managing queue."/>
 
 					<meta property="og:type" content="website"/>
 					<meta property="og:url" content="https://rewind.advil.cf/"/>
 					<meta property="og:title" content="Rewind"/>
-					<meta property="og:description" content="Music bot with support for playlists and web dashboard for managing queue."/>
+					<meta property="og:description" content="Discord music bot with support for playlists and web dashboard for managing queue."/>
 				</Head>
 				<body>
 					<Main/>
